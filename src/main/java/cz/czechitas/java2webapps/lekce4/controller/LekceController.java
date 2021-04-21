f8638092ff8240897ea52fcd2db095bce592cebc
daduelle <dani.rysava@gmail.com>
2021-04-21 22:58:57 +0200

    oprava lektorů a lekci

--- src/main/java/cz/czechitas/java2webapps/lekce4/controller/LekceController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce4/controller/LekceController.java
@@ -1,8 +1,14 @@
 package cz.czechitas.java2webapps.lekce4.controller;
 
+import cz.czechitas.java2webapps.lekce4.entity.Lekce;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 /**
  * Kontroler obsluhující sekci lekcí.
@@ -10,12 +16,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller()
 @RequestMapping("/lekce")
 public class LekceController {
+    private final List<Lekce> lekce;
 
-  /**
-   * Metoda kontroleru obsluhující adresu {@code /lekce/}. Používá šablonu {@code /templates/lekce/index.html}.
-   */
-  @GetMapping("/")
-  public String lekce() {
-    return "lekce/index";
-  }
+    public LekceController() {
+        lekce = List.of(
+                new Lekce("Lekce 1", LocalDate.of(2021, 4, 21), LocalTime.of(18, 00), LocalTime.of(20, 00), "V mechu"),
+                new Lekce("Lekce 2", LocalDate.of(2021, 4, 28), LocalTime.of(18, 00), LocalTime.of(20, 00), "V mechu"),
+                new Lekce("Lekce 3", LocalDate.of(2021, 5, 5), LocalTime.of(18, 00), LocalTime.of(20, 00), null)
+        );
+    }
+
+    /**
+     * Metoda kontroleru obsluhující adresu {@code /lekce/}. Používá šablonu {@code /templates/lekce/index.html}.
+     *
+     * @return
+     */
+    @GetMapping("/")
+    public ModelAndView lekce() {
+        ModelAndView modelAndView = new ModelAndView("lekce/index");
+        modelAndView.addObject("lekce", lekce);
+        return modelAndView;
+    }
 }
--- src/main/java/cz/czechitas/java2webapps/lekce4/entity/Lekce.java
+++ src/main/java/cz/czechitas/java2webapps/lekce4/entity/Lekce.java
@@ -7,9 +7,59 @@ import java.time.LocalTime;
  * Informace o jedné lekci.
  */
 public class Lekce {
-  private String nazev;
-  private LocalDate datum;
-  private LocalTime casOd;
-  private LocalTime casDo;
-  private String misto;
+    private String nazev;
+    private LocalDate datum;
+    private LocalTime casOd;
+    private LocalTime casDo;
+    private String misto;
+
+    public Lekce(String nazev, LocalDate datum, LocalTime casOd, LocalTime casDo, String misto) {
+        this.nazev = nazev;
+        this.datum = datum;
+        this.casOd = casOd;
+        this.casDo = casDo;
+        this.misto = misto;
+    }
+
+    public void setNazev(String nazev) {
+        this.nazev = nazev;
+    }
+
+    public void setDatum(LocalDate datum) {
+        this.datum = datum;
+    }
+
+    public void setCasOd(LocalTime casOd) {
+        this.casOd = casOd;
+    }
+
+    public void setCasDo(LocalTime casDo) {
+        this.casDo = casDo;
+    }
+
+    public void setMisto(String misto) {
+        this.misto = misto;
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public LocalDate getDatum() {
+        return datum;
+    }
+
+    public LocalTime getCasOd() {
+        return casOd;
+    }
+
+    public LocalTime getCasDo() {
+        return casDo;
+    }
+
+    public String getMisto() {
+        return misto;
+    }
+
+
 }
